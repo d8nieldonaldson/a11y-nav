7b8af81de535f736b17d54550028a7b8e2c74594
Daniel <daniel.donaldson@definitinon6.com>
2021-05-24 11:48:02 -0700

    clean up

--- main.js
+++ main.js
@@ -6,52 +6,43 @@ const globalNavListItems = globalNavList.querySelectorAll('.global-nav-list-item
 const globalNavTopLevelLinks = globalNavList.querySelectorAll('.global-nav-top-level-link');
 let isMenuOpen = false;
 
-function getParent(event){
-  return event.target.parentNode;
+function getParent(event) {
+    return event.target.parentNode;
 }
 
-function getSubmenu(parent){
-  return parent.querySelector('.submenu-list');
+function getSubmenu(parent) {
+    return parent.querySelector('.submenu-list');
 }
 
-
-
-function closeUpdateButton(button){
+function closeUpdateButton(button) {
     button.setAttribute('aria-expanded', 'false');
 }
 
-function openUpdateButton(button){
+function openUpdateButton(button) {
     button.setAttribute('aria-expanded', 'true');
 }
 
-function toggleSubmenu(submenu){
-  submenu.classList.toggle('show');
-  submenu.hidden = !submenu.hidden;
-}
-
-function closeSubmenu(submenu){
+function closeSubmenu(submenu) {
     submenu.classList.remove('show');
     submenu.hidden = !submenu.hidden;
 }
 
-function openSubmenu(submenu){
+function openSubmenu(submenu) {
     submenu.classList.add('show');
     submenu.hidden = !submenu.hidden;
 }
 
-function focusFirstChild(children){
+function focusFirstChild(children) {
     children[0].focus();
     return children[0].classList.add('focus');
 }
 
-function getChildren(parent){
-  return parent.querySelectorAll('a');
+function getChildren(parent) {
+    return parent.querySelectorAll('a');
 }
 
 
-
-
-function openUpdateNav(container, button, submenu, children){
+function openUpdateNav(container, button, submenu, children) {
     container.classList.add('expanded');
     openUpdateButton(button)
     openSubmenu(submenu);
@@ -59,38 +50,48 @@ function openUpdateNav(container, button, submenu, children){
     isMenuOpen = true;
 }
 
-function closeUpdateNav(container, button, submenu){
+function closeUpdateNav(container, button, submenu) {
     container.classList.remove('expanded');
     closeUpdateButton(button)
     closeSubmenu(submenu)
     isMenuOpen = false;
 }
 
+function findOpenMenuElements(container) {
+    const openParent = container.querySelector('.expanded');
+    const openButton = openParent.querySelector('button');
+    const openSubmenu = getSubmenu(openParent);
+    const openSubmenuChildren = getChildren(openSubmenu);
+    return [openParent, openButton, openSubmenu, openSubmenuChildren];
+}
+
+function findNewMenuElements(event) {
+    const parent = getParent(event);
+    const button = parent.querySelector('button');
+    const submenu = getSubmenu(parent);
+    const children = getChildren(submenu);
+    return [parent, button, submenu, children];
+}
 
 submenuToggles.forEach(toggle => {
     toggle.addEventListener('click', e => {
-      let parent = getParent(e);
-      let submenu = getSubmenu(parent);
-      let children = getChildren(submenu);
-      let button = parent.querySelector('button');
-        if(isMenuOpen && parent.classList.contains('expanded')){
-            console.log(`just close that shit`);
+        const [parent, button, submenu, children] = findNewMenuElements(e);
+        if (isMenuOpen && parent.classList.contains('expanded')) {
+            console.log(`just close that shit update betterized`);
             return closeUpdateNav(parent, button, submenu);
         }
-        if(isMenuOpen && !parent.classList.contains('expanded')){
+        if (isMenuOpen && !parent.classList.contains('expanded')) {
             console.log(`it is complicated`);
-            let currentlyOpenParent = globalNavList.querySelector('.expanded');
-            let currentlyOpenButton = currentlyOpenParent.querySelector('button');
-            let currentlyOpenSubmenu = currentlyOpenParent.querySelector('ul.submenu-list');
-            closeUpdateNav(currentlyOpenParent, currentlyOpenButton, currentlyOpenSubmenu);
+            const [openParent, openButton, openSubmenu] = findOpenMenuElements(globalNavList);
+            closeUpdateNav(openParent, openButton, openSubmenu);
             return setTimeout(openUpdateNav(parent, button, submenu, children), 0);
-          
+
         }
-        if(!isMenuOpen){
+        if (!isMenuOpen) {
             console.log(`just open that shit`);
             return openUpdateNav(parent, button, submenu, children);
         }
- 
+
     });
 });
 
@@ -101,16 +102,14 @@ globalNavListItems.forEach(item => {
         let submenu = getSubmenu(parent);
         let children = getChildren(submenu);
         let button = parent.querySelector('button');
-        if(isMenuOpen && parent.classList.contains('expanded')){
-            console.log('already open, muthafuckahhhh');
+        if (isMenuOpen && parent.classList.contains('expanded')) {
+            console.log('already open');
             return;
         }
-        if(isMenuOpen && !parent.classList.contains('expanded')){
-            console.log('it is complicated again');
-            let currentlyOpenParent = globalNavList.querySelector('.expanded');
-            let currentlyOpenButton = currentlyOpenParent.querySelector('button');
-            let currentlyOpenSubmenu = currentlyOpenParent.querySelector('ul.submenu-list');
-            closeUpdateNav(currentlyOpenParent, currentlyOpenButton, currentlyOpenSubmenu);
+        if (isMenuOpen && !parent.classList.contains('expanded')) {
+            console.log('it is complicated again as well');
+            const [openParent, openButton, openSubmenu] = findOpenMenuElements(globalNavList);
+            closeUpdateNav(openParent, openButton, openSubmenu);
             return setTimeout(openUpdateNav(parent, button, submenu, children), 0);
         }
         return openUpdateNav(parent, button, submenu, children);
@@ -123,7 +122,7 @@ globalNavListItems.forEach(item => {
         let parent = e.target;
         let submenu = getSubmenu(parent);
         let button = parent.querySelector('button');
-        if(isMenuOpen && parent.classList.contains('expanded')){
+        if (isMenuOpen && parent.classList.contains('expanded')) {
             return closeUpdateNav(parent, button, submenu);
         }
     });
@@ -132,12 +131,10 @@ globalNavListItems.forEach(item => {
 
 globalNavTopLevelLinks.forEach(link => {
     link.addEventListener('focus', e => {
-        if(isMenuOpen){
-            console.log('when link gets focus, close the open menu');
-            let currentlyOpenParent = globalNavList.querySelector('.expanded');
-            let currentlyOpenButton = currentlyOpenParent.querySelector('button');
-            let currentlyOpenSubmenu = currentlyOpenParent.querySelector('ul.submenu-list');
-            return closeUpdateNav(currentlyOpenParent, currentlyOpenButton, currentlyOpenSubmenu);
+        if (isMenuOpen) {
+            console.log('when link gets focus, close the open menu, updated, again');
+            const [openParent, openButton, openSubmenu] = findOpenMenuElements(globalNavList);
+            return closeUpdateNav(openParent, openButton, openSubmenu);
 
         }
     });
@@ -146,75 +143,60 @@ globalNavTopLevelLinks.forEach(link => {
 
 document.addEventListener('keydown', function(e) {
     console.log(e.code); // User presses enter on their main keyboard - Enter
-    if(e.code === 'ArrowUp'){
-        console.log('up arrow pressedjsafkjdf');
-        if(isMenuOpen){
-            
-            let currentlyOpenParent = globalNavList.querySelector('.expanded');
-            let currentlyOpenSubmenu = currentlyOpenParent.querySelector('ul.submenu-list'); 
-            let children = getChildren(currentlyOpenSubmenu);
-            let lastChild = children[children.length -1];
-            let currentIndex = 0;
-            let nextIndex = 0;
+    if (isMenuOpen) {
+        const [openParent, openButton, openSubmenu, children] = findOpenMenuElements(globalNavList);
+        const firstChild = children[0];
+        const lastChild = children[children.length - 1];
+        let currentIndex = 0;
+        let nextIndex = 0;
+        if (e.code === 'ArrowUp') {
+            console.log('up arrow pressed, streamlined j');
             children.forEach((child, index) => {
-                if(child.classList.contains('focus')){
+                if (child.classList.contains('focus')) {
                     currentIndex = index;
                 };
             });
-         
-            if(currentIndex === 0){
-                children[0].classList.remove('focus');
-                children[children.length -1].focus();
-                return children[children.length -1].classList.add('focus');
+
+            if (currentIndex === 0) {
+                console.log(`new way`);
+                firstChild.classList.remove('focus');
+                lastChild.focus();
+                return lastChild.classList.add('focus');
 
             }
             nextIndex = currentIndex - 1;
             children[currentIndex].classList.remove('focus');
             children[nextIndex].classList.add('focus');
             return children[nextIndex].focus();
-
         }
-    }
-    if(e.code === 'ArrowDown'){
-        console.log('downtown arrow');
-        if(isMenuOpen){
-            
-            let currentlyOpenParent = globalNavList.querySelector('.expanded');
-            let currentlyOpenSubmenu = currentlyOpenParent.querySelector('ul.submenu-list'); 
-            let children = getChildren(currentlyOpenSubmenu);
-            let lastChild = children[children.length -1];
-            let currentIndex = 0;
+        if (e.code === 'ArrowDown') {
+            console.log('downtown arrow k');
             children.forEach((child, index) => {
-                if(child.classList.contains('focus')){
+                if (child.classList.contains('focus')) {
                     currentIndex = index;
                 };
             });
-            console.log(`arrow key down and ${currentIndex}`);
-            console.log(`last child: ${lastChild}`);
-            if(currentIndex === children.length - 1){
+            console.log(`arrow key down and ${currentIndex} k`);
+            if (currentIndex === children.length - 1) {
                 children[currentIndex].classList.remove('focus');
-                children[0].focus();
-                return children[0].classList.add('focus');
+                firstChild.focus();
+                return firstChild.classList.add('focus');
 
             }
             nextIndex = currentIndex + 1;
-            console.log(`down arrow and nextIndex: ${nextIndex}`);
+            console.log(`down arrow and nextIndex: ${ nextIndex } k`);
             children[currentIndex].classList.remove('focus');
             children[nextIndex].classList.add('focus');
             return children[nextIndex].focus();
+
+        }
+        if (e.code === 'Escape') {
+            console.log('ESC to close k');
+            closeUpdateNav(openParent, openButton, openSubmenu);
+            return openButton.focus();
         }
     }
-    if(e.code === 'Escape'){
-        console.log('escape key pressed');
-        if(isMenuOpen){
-            console.log('ESC to close');
-            let currentlyOpenParent = globalNavList.querySelector('.expanded');
-            let currentlyOpenButton = currentlyOpenParent.querySelector('button');
-            let currentlyOpenSubmenu = currentlyOpenParent.querySelector('ul.submenu-list');
-            closeUpdateNav(currentlyOpenParent, currentlyOpenButton, currentlyOpenSubmenu);
-            return currentlyOpenButton.focus();
-        }
-    }
+
 });
 
 // click anywhere outside of open menu to close
